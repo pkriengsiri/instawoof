bd88010bc599a2f3dc11a96e6b308034f01b78bd
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-04-09 18:41:41 -0400

    Added camera functionality to Add component

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,12 +1,95 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
+import { Camera } from "expo-camera";
 
-export default function Add() {
-    return (
-        <View>
-            <Text>
-                Add is here
-            </Text>
-        </View>
-    )
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image,setImage] = useState(null);
+
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const takePicture = async () => {
+      console.log("clicked the take picture button")
+      if(camera) {
+          const data = await camera.takePictureAsync(null);
+          setImage(data.uri);
+      }
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.cameraContainer}>
+        <Camera
+          style={styles.camera}
+          type={type}
+          ratio={"1:1"}
+          ref={(ref) => setCamera(ref)}
+        />
+      </View>
+      <Button
+        style={styles.button}
+        title="Flip Image"
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          );
+        }}
+      />
+      <Button
+        title="Take Picture"
+        onPress={() => {
+          takePicture();
+        }}
+      />
+      {image && <Image source={{uri:image}} style={styles.image}/>}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  cameraContainer: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  camera: {
+    flex: 1,
+    aspectRatio: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
+  },
+  image: {
+      flex: 1
+  }
+});
