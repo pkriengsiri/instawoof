4c1914d94df28d83ae8ed69bd98837fb5a7f804d
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-04-09 11:01:58 -0400

    Installed react-native-dotenv, updated babel config, added firebase config to App.js

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
+// 3rd party imports
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-
-
+import * as firebase from "firebase";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+
+
+// local imports
 import Landing from "./components/auth/Landing";
+import {FIREBASE_API_KEY} from "react-native-dotenv";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: {FIREBASE_API_KEY},
+  authDomain: "instawoof-2c2fc.firebaseapp.com",
+  projectId: "instawoof-2c2fc",
+  storageBucket: "instawoof-2c2fc.appspot.com",
+  messagingSenderId: "491558044435",
+  appId: "1:491558044435:web:04072f5d81ec50ead290e6",
+  measurementId: "G-QW2EW0KYEM"
+};
 
 const Stack = createStackNavigator();
 
